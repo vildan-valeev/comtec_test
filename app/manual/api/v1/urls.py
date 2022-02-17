72e91b5c2b1bef62bd887d9f5fb43c427563e3d8
vildan-valeev <vildan.maydanovich.valeev@gmail.com>
2022-02-18 02:18:11 +0500

    add custom urlpattern + converter

--- app/manual/api/v1/urls.py
+++ app/manual/api/v1/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, register_converter
 
-from manual.api.v1.views import ManualList
+from manual.api.v1.views import ManualList, ManualListAsOfDate
+from manual.serices.url_converter import DateConverter
+
+register_converter(DateConverter, 'dd-mm-yyyy')
 
 urlpatterns = [
     path('manual/', ManualList.as_view()),
-    path('manual/current/<fds>', ManualList.as_view()),
+    path('manual/current/<dd-mm-yyyy:date>/', ManualListAsOfDate.as_view()),
     # path('item/', ItemCreateList.as_view()),
     # path('item/<int:pk>/', ItemDetail.as_view()),
 ]
---
+++ app/manual/serices/url_converter.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+class DateConverter:
+    regex = '(?:0[1-9]|[12][0-9]|3[01])-(?:0[1-9]|1[0-2])-(?:20[12345][0-9])'
+    # расчет от 01-01-2010 до 31-12-2059
+
+    def to_python(self, value):
+        return datetime.strptime(value, "%d-%m-%Y")
+
+    def to_url(self, value):
+        return value.strftime("%d-%m-%Y")
